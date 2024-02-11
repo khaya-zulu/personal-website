c9ef6104bf424f7b17e95c1aa5aab7ac0e6b71fc
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2024-02-11 12:33:36 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: "server",
   integrations: [
     mdx(),
     vercel({
